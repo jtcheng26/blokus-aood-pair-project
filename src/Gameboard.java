f0e81808dc1e4bd6e4eeeab9ae98ad425754d902
Matthew Won <74610837+NekoNekoNyahChan@users.noreply.github.com>
2021-01-03 14:35:28 -0500

    See description below
    probably finished the playerOut method

--- src/Gameboard.java
+++ src/Gameboard.java
@@ -2,41 +2,51 @@ import java.util.List;
 
 public class Gameboard {
 	private int[][] grid;
+	private int[][] instantaneousGrid;
 	private int gridSize;
+	
 	Gameboard (int numPlayers) {
 		if (numPlayers == 2) {
 			grid = new int[15][15];
+			instantaneousGrid = new int[15][15];
 			for (int hor=0; hor < 15; hor++) {
 				for (int vert=0; vert < 15; vert++) {
 					grid[hor][vert] = 0;
+					instantaneousGrid[hor][vert] = 0;
 				}
 			}
 			gridSize = 15;
 		}
 		if (numPlayers == 3) {
 			grid = new int [18][18];
+			instantaneousGrid = new int[18][18];
 			for (int hor=0; hor < 18; hor++) {
 				for (int vert=0; vert < 18; vert++) {
 					grid[hor][vert] = 0;
+					instantaneousGrid[hor][vert] = 0;
 				}
 			}
 			gridSize = 18;
 		}
 		if (numPlayers == 4) {
 			grid = new int [20][20];
+			instantaneousGrid = new int[20][20];
 			for (int hor=0; hor < 20; hor++) {
 				for (int vert=0; vert < 20; vert++) {
 					grid[hor][vert] = 0;
+					instantaneousGrid[hor][vert] = 0;
 				}
 			}
 			gridSize = 20;
 		}
 	}
+	
 	public void placePiece (GamePiece piece, Player player) {
 		GamePiece currentPiece = player.getPiece(player.getPiecesLeft().indexOf(piece));
 		if (isValid(piece,player)) {
 			for (int i = 0; i < currentPiece.getPieceCoordinates().size(); i++) {
 				grid[currentPiece.getPieceCoordinates().get(i).getX()][currentPiece.getPieceCoordinates().get(i).getY()] = player.getID();
+				instantaneousGrid[currentPiece.getPieceCoordinates().get(i).getX()][currentPiece.getPieceCoordinates().get(i).getY()] = player.getID();
 			}
 			player.getPiecesLeft().remove(piece);
 			player.getPiecesUsed().add(piece);
@@ -130,58 +140,81 @@ public class Gameboard {
 			returnBoolean = true;
 		} else {
 			for (int i = 0; i < player.getPiecesLeft().size(); i++) {
-				for (int j = 0; j < player.getCornerPositions().size(); j++) {
-					player.getPiecesLeft().get(i).setLocation(player.getCornerPositions().get(j));
-					/*
-					 * insert body
-					 */
-					if (!isValid(player.getPiecesLeft().get(i), player)) {
-						player.getPiecesLeft().get(i).rotatePiece();
+				for (int j = 0; j < player.getPiecesLeft().get(i).getPieceCoordinates().size(); j++) {
+					for (int k = 0; k < player.getCornerPositions().size(); k++) {
+						player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, player.getCornerPositions().get(k));
 						if (!isValid(player.getPiecesLeft().get(i), player)) {
 							player.getPiecesLeft().get(i).rotatePiece();
 							if (!isValid(player.getPiecesLeft().get(i), player)) {
 								player.getPiecesLeft().get(i).rotatePiece();
 								if (!isValid(player.getPiecesLeft().get(i), player)) {
 									player.getPiecesLeft().get(i).rotatePiece();
-									player.getPiecesLeft().get(i).flipPiece();
 									if (!isValid(player.getPiecesLeft().get(i), player)) {
 										player.getPiecesLeft().get(i).rotatePiece();
+										player.getPiecesLeft().get(i).flipPiece();
 										if (!isValid(player.getPiecesLeft().get(i), player)) {
 											player.getPiecesLeft().get(i).rotatePiece();
 											if (!isValid(player.getPiecesLeft().get(i), player)) {
 												player.getPiecesLeft().get(i).rotatePiece();
 												if (!isValid(player.getPiecesLeft().get(i), player)) {
-													returnBoolean = true;
+													player.getPiecesLeft().get(i).rotatePiece();
+													if (!isValid(player.getPiecesLeft().get(i), player)) {
+														returnBoolean = true;
+													} else {
+														player.getPiecesLeft().get(i).rotatePiece();
+														player.getPiecesLeft().get(i).flipPiece();
+														player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, new Position(0,0));
+														returnBoolean = false;
+													}
 												} else {
+													player.getPiecesLeft().get(i).rotatePiece();
+													player.getPiecesLeft().get(i).rotatePiece();
+													player.getPiecesLeft().get(i).flipPiece();
+													player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, new Position(0,0));
 													returnBoolean = false;
 												}
 											} else {
+												player.getPiecesLeft().get(i).rotatePiece();
+												player.getPiecesLeft().get(i).rotatePiece();
+												player.getPiecesLeft().get(i).rotatePiece();
+												player.getPiecesLeft().get(i).flipPiece();
+												player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, new Position(0,0));
 												returnBoolean = false;
 											}
 										} else {
+											player.getPiecesLeft().get(i).flipPiece();
+											player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, new Position(0,0));
 											returnBoolean = false;
 										}
 									} else {
+										player.getPiecesLeft().get(i).rotatePiece();
+										player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, new Position(0,0));
 										returnBoolean = false;
 									}
 								} else {
+									player.getPiecesLeft().get(i).rotatePiece();
+									player.getPiecesLeft().get(i).rotatePiece();
+									player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, new Position(0,0));
 									returnBoolean = false;
 								}
 							} else {
+								player.getPiecesLeft().get(i).rotatePiece();
+								player.getPiecesLeft().get(i).rotatePiece();
+								player.getPiecesLeft().get(i).rotatePiece();
+								player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, new Position(0,0));
 								returnBoolean = false;
 							}
 						} else {
+							player.getPiecesLeft().get(i).setPieceCoordinateLocation(j, new Position(0,0));
 							returnBoolean = false;
 						}
-					} else {
-						returnBoolean = false;
 					}
-					player.getPiecesLeft().get(i).setLocation(new Position(0,0));
 				}
 			}
 		}
 		return returnBoolean;
 	}
+	
 	public void rotateBoard() {
 		int[][] newGrid = new int[gridSize][gridSize];
 		for (int i=0;i<gridSize;i++) {
@@ -191,10 +224,20 @@ public class Gameboard {
 		}
 		this.grid = newGrid;
 	}
+	
 	public void print(Gameboard board) {
 		for (int i=0; i < board.gridSize; i++) {
 			for (int j=0; j < board.gridSize; j++) {
 				System.out.printf("%-2s", board.grid[j][board.gridSize - i - 1] == 0 ? "." : board.grid[j][board.gridSize - i - 1]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public void printInstantaneousGrid(Gameboard board) {
+		for (int i=0; i < board.gridSize; i++) {
+			for (int j=0; j < board.gridSize; j++) {
+				System.out.printf("%-2s", board.instantaneousGrid[j][board.gridSize - i - 1] == 0 ? "." : board.instantaneousGrid[j][board.gridSize - i - 1]);
 			}
 			System.out.println();
 		}
@@ -213,6 +256,6 @@ public class Gameboard {
 			System.out.println(player1.getCornerPositions().get(i).getX()+","+player1.getCornerPositions().get(i).getY());
 		}
 		//board.placePiece(player2.getPiece(0), player2);
-		board.print(board);
+		board.printInstantaneousGrid(board);
 	}
 }
