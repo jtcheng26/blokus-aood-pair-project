c2fc023471ec51c6c3466ec72a398e1673ce8feb
jtcheng26 <jtcheng226@gmail.com>
2021-01-11 10:57:53 -0500

    wasd keys

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -180,19 +180,19 @@ public class GameScreen extends JPanel implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 	    int key = e.getKeyCode();
 
-	    if (key == KeyEvent.VK_LEFT) {
+	    if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) {
 	        onLeftArrow();
 	    }
 
-	    if (key == KeyEvent.VK_RIGHT) {
+	    if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) {
 	        onRightArrow();
 	    }
 
-	    if (key == KeyEvent.VK_UP) {
+	    if (key == KeyEvent.VK_UP || key == KeyEvent.VK_W) {
 	        onUpArrow();
 	    }
 
-	    if (key == KeyEvent.VK_DOWN) {
+	    if (key == KeyEvent.VK_DOWN || key == KeyEvent.VK_S) {
 	        onDownArrow();
 	    }
 	    
